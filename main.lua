eec2cfa49f55f1d5a61640dfa28c323f1658b412
ravendsss <smoothry86@gmail.com>
2026-04-29 23:28:58 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,9 +5,9 @@ if placeId == 142823291 then
 elseif placeId == 77747658251236 then
     -- Sailor Piece Dupe
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ravendsss/SatyHub/refs/heads/main/sailorpiecedupe.lua", true))()
-elseif placeId == 109983668079237 then
+elseif placeId == 109983668079237 or placeId == 96342491571673 then
     -- Steal A Brainrot
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ravendsss/SatyHub/refs/heads/main/sab.lua", true))()
 else
-    game.Players.LocalPlayer:Kick("This game is not supported! Please use at MM2/Sailor Piece/Steal A Brainrot!!")
+    game.Players.LocalPlayer:Kick("This game is not supported! Please use at MM2 / Sailor Piece / Steal A Brainrot!!")
 end
